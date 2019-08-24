967e0c967ec791410ee0ba05e245638917d4f8e9
dongkesi <skd2278@gmail.com>
2019-08-24 20:36:33 +0800

    Projects 04 Done

--- nand2tetris/projects/04/fill/Fill.asm
+++ nand2tetris/projects/04/fill/Fill.asm
@@ -12,3 +12,69 @@
 // the screen should remain fully clear as long as no key is pressed.
 
 // Put your code here.
+
+  @key_status
+  M=0     // keep last key status
+  
+  @8192
+  D=A
+  @n
+  M=D     // n = 8192;
+
+(KEY_SCAN)
+  @KBD
+  D=M
+  @key_status
+  D=D-M
+  @KEY_SCAN
+  D;JEQ   // if key not change, continue scan
+
+  @KBD
+  D=M
+  @key_status
+  M=D     // store the key status
+
+(SCREEN_FLUSH_BEGIN)
+  @0
+  D=A
+  @i
+  M=D     // i = 0;
+
+(LOOP)
+  @i
+  D=M
+  @n
+  D=D-M
+  @WINDOW_FLUSH_DONE
+  D;JEQ
+  @key_status
+  D=M         // key_status == 0; WHITE
+  @WHITE
+  D;JEQ
+
+(BLACK)
+  @SCREEN
+  D=A
+  @i
+  A=D+M
+  M=-1
+  @NEXT
+  0;JMP
+
+(WHITE)
+  @SCREEN
+  D=A
+  @i
+  A=D+M
+  M=0
+
+(NEXT)
+  @i
+  M=M+1
+
+  @LOOP
+  0;JMP
+
+(WINDOW_FLUSH_DONE)
+  @KEY_SCAN
+  0;JMP
--- nand2tetris/projects/04/mult/mult.asm
+++ nand2tetris/projects/04/mult/mult.asm
@@ -7,3 +7,49 @@
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
 // Put your code here.
+
+  @product
+  M=0       // product = 0
+
+(IS_R0_R1_EQ_0)
+  @R0
+  D=M
+  @MULTI_DONE
+  D;JEQ     // if R0 is 0, goto MULTI_DONE
+  @R1
+  D=M
+  @MULTI_DONE
+  D;JEQ     // if R1 is 0, goto MULTI_DONE
+
+(MULTI_BEGIN)
+  @R0
+  D=M
+  @n
+  M=D       // n = R0
+
+(LOOP)
+  @n
+  D=M    
+  @MULTI_DONE
+  D;JEQ      // if (n==0) goto MULTI_DONE
+  
+  @R1
+  D=M
+  @product
+  M=D+M
+
+  @n
+  M=M-1      // n--
+  
+  @LOOP
+  0;JMP
+
+(MULTI_DONE)
+  @product
+  D=M
+  @R2
+  M=D       // R2 = product
+
+(END)
+  @END
+  0;JMP
